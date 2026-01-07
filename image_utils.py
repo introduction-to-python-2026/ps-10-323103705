7f3009ede3dcee092f5e4b246baa4f9d62429b2d
323103705 <rotemr2001@gmail.com>
2026-01-07 11:52:42 +0200

    Update edge_detection function

--- image_utils.py
+++ image_utils.py
@@ -8,4 +8,15 @@ def load_image(path):
     return image
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    # convert into a grayscale
+    gray_image = np.mean(image, axis=2)
+    # creating a filter for vertical changes
+    KernelY= np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    # creating a filter for horizontal changes 
+    KernelX = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    # applaying each filter on the grayscale
+    edgeY = convolve2d(gray_image, KernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(gray_image, KernelX, mode="same", boundary='fill', fillvalue=0)
+    # new array that combines both edges
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
