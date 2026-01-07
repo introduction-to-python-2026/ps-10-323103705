3378ea7bcd458a6f8f11b5d0510dc2ee6be46491
323103705 <rotemr2001@gmail.com>
2026-01-07 11:20:21 +0200

    update load_image function

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,9 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    image = Image.open(path)
+    image = np.array(image)
+    return image
 
 def edge_detection(image):
     pass # Replace the `pass` with your code
